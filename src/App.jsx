c04e18e042b735b6a7fb60ad744624541a9cddea
LornahMunene <munenelornah9@gmail.com>
2024-04-28 13:17:47 +0300

    bank-of-flatiron

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+import React,{useState} from 'react';
+
+function App() {
+  const [transactions,setTransactions]=useState([]);
+  const addTransaction =(transaction)=>{
+    setTransactions([...transactions,transaction]);
+  }
+  return (
+    <div >
+      <h1 className="App">The Royal Bank of Flatiron</h1>
+      <Form addTransaction={addTransaction}/>
+      
+    </div>
+  
+  
+  );
+}
+
+export default App;
